8602e3a77cad979e74cdbcb7910857d4f0c572a9
Михаил Иванов <m.ivanych@gmail.com>
2012-07-11 18:58:35 +0400

    Сделано чтение списка файлов

--- Apptest/AppDelegate.m
+++ Apptest/AppDelegate.m
@@ -36,10 +36,40 @@
         // Список выбранных файлов
         NSArray *files = [openDlg URLs];
         
+        // Обрабатываем список
+        for(NSURL *url in files) {
+            NSLog(@"%@", url);
+            
+            // Берем из URL только путь к файлу
+            NSString *path = [url path];
+            
+            // Объект файл-менеджера
+            NSFileManager *fileManager = [[NSFileManager alloc] init];
+            // Флаг каталога
+            BOOL isDir;
+            
+            // Если файл является каталогом
+            if ([fileManager fileExistsAtPath:path isDirectory:&isDir] && isDir) {
+                NSLog(@"dir: %@", path);
+                
+                //Читаем содержимое каталога
+                NSArray * dir = [fileManager contentsOfDirectoryAtPath:path error:nil];
+                
+                // Вывод содержимого каталога
+                for (NSArray *d in dir) {
+                    NSLog(@"%@", d);
+                }
+            }
+            // Если файл является файлом
+            else {
+                NSLog(@"file: %@", path);
+            }
+        }
+        
         // Показать выбранные файлы
-        NSAlert *alert = [[NSAlert alloc] init];
-        [alert setMessageText:[files componentsJoinedByString:@",\n"]];
-        [alert runModal];
+        //NSAlert *alert = [[NSAlert alloc] init];
+        //[alert setMessageText:[files componentsJoinedByString:@",\n"]];
+        //[alert runModal];
     }
 }
 
